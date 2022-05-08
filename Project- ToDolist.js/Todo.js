147c178e7c851c125f8e27ab60230b4707467a35
Aakash644 <92630714+Aakash644@users.noreply.github.com>
2022-05-08 20:08:04 +0530

    Update Todo.js

--- Project- ToDolist.js/Todo.js
+++ Project- ToDolist.js/Todo.js
@@ -11,11 +11,13 @@ if(input=="list" ){
     }
 else if(input=="add"){
     let add=prompt("What would you like to add?");
-    todolist.push(add); 
+    todolist.push(add);  
+    console.log(`${add} was added to the list`);
 } 
 else if(input=="delete"){
     let index=prompt("What is the Index number of removovable item?");
-    todolist.splice(index,1);
+    const deleted=todolist.splice(index,1); 
+    console.log(`${deleted[0]} was deleted from the list`); 
 }
  
 
